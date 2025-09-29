936081b8571f90ff7890a9875cf524f7c7c4c998
james <huytn@iceteasoftware.com>
2025-09-29 17:23:44 +0700

    feat: cookie

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,11 +1,10 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
 export async function GET() {
   const cookie = await cookies();
   cookie.set("custom-cookie", Math.random().toString());
-  return redirect("/");
+  return redirect("/cookie");
 }
